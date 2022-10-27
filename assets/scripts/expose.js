64b3343fd7f4d38e77c0532bf624b88a3d8dde54
Bradley Chester <68435346+bchester102@users.noreply.github.com>
2022-10-27 12:13:59 -0700

    Finish expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,31 +2,58 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const jsConfetti = new JSConfetti();
 function init() {
   // TODO
   const selectSound = document.getElementById("horn-select");
   const soundImage = document.querySelector("img");
   const soundFile = document.querySelector("audio");
 
+  const playButton = document.querySelector("button");
+  let confetti = false;
+
+  const volumeSlider = document.querySelector("input");
+  const volumeImage = document.querySelector("#volume-controls>img");
+
   selectSound.addEventListener("change", (event) => {
     if (event.target.value == "air-horn") {
       soundImage.src = "assets/images/air-horn.svg";
       soundFile.src = "assets/audio/air-horn.mp3"
+      confetti = false;
     }
     else if (event.target.value == "car-horn") {
       soundImage.src = "assets/images/car-horn.svg";
       soundFile.src = "assets/audio/car-horn.mp3"
+      confetti = false;
     }
     else if (event.target.value == "party-horn") {
       soundImage.src = "assets/images/party-horn.svg";
       soundFile.src = "assets/audio/party-horn.mp3"
+      confetti = true;
     }
   });
 
-  const playButton = document.querySelector("button");
-
   playButton.addEventListener("click", () => {
-    soundFile.volume = 0.5;
     soundFile.play();
-  })
+    if (confetti) jsConfetti.addConfetti();
+  });
+
+  volumeSlider.addEventListener("input", (event) => {
+    const volume = event.target.value;
+    soundFile.volume = volume/100;
+    if (volume <= 0){ 
+      volumeImage.src = "assets/icons/volume-level-0.svg";
+    }
+    else if (1 <= volume && volume < 33){ 
+      volumeImage.src = "assets/icons/volume-level-1.svg";
+    }
+    else if (33 <= volume && volume < 67){ 
+      volumeImage.src = "assets/icons/volume-level-2.svg";
+    }
+    else if (volume >= 67){ 
+      volumeImage.src = "assets/icons/volume-level-3.svg";
+    }
+  });
+
+
 }
