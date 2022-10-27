b4abd3f1e3573dae4d855410bb6a6ef62327beec
Bradley Chester <68435346+bchester102@users.noreply.github.com>
2022-10-27 11:29:28 -0700

    Added image change functionality

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,19 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let selectSound = document.getElementById("horn-select");
+  let soundImage = document.querySelector("img");
+  //let volumeImage
+
+  selectSound.addEventListener("change", (event) => {
+    if (event.target.value == "air-horn") {
+      soundImage.src = "assets/images/air-horn.svg";
+    }
+    else if (event.target.value == "car-horn") {
+      soundImage.src = "assets/images/car-horn.svg";
+    }
+    else if (event.target.value == "party-horn") {
+      soundImage.src = "assets/images/party-horn.svg";
+    }
+  });
 }
