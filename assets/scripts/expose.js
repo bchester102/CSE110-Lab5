ff8306e929d89ed41d80655ba4cad710a326a27b
Bradley Chester <68435346+bchester102@users.noreply.github.com>
2022-10-27 11:41:42 -0700

    Added play functionality

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,19 +4,29 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  let selectSound = document.getElementById("horn-select");
-  let soundImage = document.querySelector("img");
-  //let volumeImage
+  const selectSound = document.getElementById("horn-select");
+  const soundImage = document.querySelector("img");
+  const soundFile = document.querySelector("audio");
 
   selectSound.addEventListener("change", (event) => {
     if (event.target.value == "air-horn") {
       soundImage.src = "assets/images/air-horn.svg";
+      soundFile.src = "assets/audio/air-horn.mp3"
     }
     else if (event.target.value == "car-horn") {
       soundImage.src = "assets/images/car-horn.svg";
+      soundFile.src = "assets/audio/car-horn.mp3"
     }
     else if (event.target.value == "party-horn") {
       soundImage.src = "assets/images/party-horn.svg";
+      soundFile.src = "assets/audio/party-horn.mp3"
     }
   });
+
+  const playButton = document.querySelector("button");
+
+  playButton.addEventListener("click", () => {
+    soundFile.volume = 0.5;
+    soundFile.play();
+  })
 }
